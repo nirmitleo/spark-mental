734bb92aaf25ccfb1c1ffdd54d92dc3ec3cae582
nirmitleo <nirmit@invideo.io>
2021-05-02 14:50:10 +0530

    more changes

--- src/app/game/game/game.component.ts
+++ src/app/game/game/game.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { Question } from "src/app/question.model";
+import { Component, OnInit } from '@angular/core';
+import { Question } from 'src/app/question.model';
 @Component({
-  selector: "app-game",
-  templateUrl: "./game.component.html",
-  styleUrls: ["./game.component.scss"],
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit {
   GAME_LENGTH = 10;
-  MIN_DIFFICULTY = 2;
-  MAX_DIFFICULTY = 20;
+  MIN_DIFFICULTY = 9;
+  MAX_DIFFICULTY = 13;
 
   correct = 0;
   questions: Question[] = [];
@@ -27,7 +27,7 @@ export class GameComponent implements OnInit {
     console.log(this.correct);
 
     if (this.correct === this.GAME_LENGTH) {
-      console.log("game over!");
+      console.log('game over!');
       setTimeout(() => {
         this.initializeGame();
       }, 2000);
@@ -35,11 +35,10 @@ export class GameComponent implements OnInit {
   }
 
   private initializeGame() {
+    this.correct = 0;
     this.questions = [];
     for (let i = 0; i < this.GAME_LENGTH; i++) {
-      const operand1 = this.getOperand();
-      const operand2 = this.getOperand();
-
+      const [operand1, operand2] = this.getIntegerOperand();
       const question: Question = {
         operand1,
         operand2,
@@ -50,10 +49,21 @@ export class GameComponent implements OnInit {
     }
   }
 
-  private getOperand(): number {
-    const value =
-      Math.random() * (this.MAX_DIFFICULTY - this.MIN_DIFFICULTY + 1) +
-      this.MIN_DIFFICULTY;
-    return Math.trunc(value);
+  private getIntegerOperand(): [number, number] {
+    const operand1 = this.getIntegerRandomValue(2, 20);
+    const operand2 = this.getIntegerRandomValue(this.MIN_DIFFICULTY, this.MAX_DIFFICULTY);
+
+    const integerOperands = this.shuffle(operand1, operand2);
+    return integerOperands;
+  }
+
+  private shuffle(operand1: number, operand2: number): [number, number] {
+    return Math.trunc(Math.random() * 100) % 2 === 0 ? [operand1, operand2] : [operand2, operand1];
+  }
+
+  private getIntegerRandomValue(minRange: number, maxRange: number) {
+    let randomInt = Math.random() * (maxRange - minRange + 1) + minRange;
+    randomInt = Math.trunc(randomInt);
+    return randomInt;
   }
 }
